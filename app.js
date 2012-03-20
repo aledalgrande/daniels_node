fbf31bdcf4356b00078d6014aaee2e825fdef6c0
aledalgrande <aledalgrande@gmail.com>
2012-03-20 20:50:51 +0000

    port conf fix for heroku

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
