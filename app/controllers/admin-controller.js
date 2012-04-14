bbfad847ea040028ab392b947c4f373f4af39372
aledalgrande <aledalgrande@gmail.com>
2012-04-14 01:25:18 +0100

    admin index page with events

--- app/controllers/admin-controller.js
+++ app/controllers/admin-controller.js
@@ -3,8 +3,7 @@ var Event = mongoose.model('Event');
 module.exports = function(app){
 
 	app.get('/admin', express.basicAuth(username, password), function(req, res){
-		var query = Event.find({});
-		var events = query.where('createdAt').gte(new Date()).run(function (err, docs) {
+		var query = Event.find({}).run(function (err, docs) {
 		  res.render('admin/index', { title: 'Daniels ∙ Admin', events: docs });
 		});
 	});
