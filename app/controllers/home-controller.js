d70dfe1cb1b70be18450729f95e60a7f64ec939b
aledalgrande <aledalgrande@gmail.com>
2012-04-15 16:06:46 +0100

    event linking in the homepage, map directions

--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -1,9 +1,15 @@
+var Event = mongoose.model('Event')
+
 module.exports = function(app){
 	
 	app.get('/', function(req, res){
-		res.render('index', {
-			title: 'Daniels'
-		});
+		Event.where('end').gte(new Date()).sort('createdAt', -1).limit(1).populate('venue').run(function(err, docs){
+			if (err) res.render('DB error', 500);
+			res.render('index', {
+				title: 'Daniels'
+				, event: docs.length > 0 ? docs[0] : new Event({})
+			});
+		})
 	});
 
 }
