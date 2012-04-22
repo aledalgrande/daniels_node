f09b68d19044b38a51bb6902be583d25a74f9b07
aledalgrande <aledalgrande@gmail.com>
2012-04-22 19:18:21 +0100

    ui stability fix

--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -3,13 +3,13 @@ var Event = mongoose.model('Event')
 module.exports = function(app){
 	
 	app.get('/', function(req, res){
-		Event.where('end').gte(new Date()).sort('createdAt', -1).limit(1).populate('venue').run(function(err, docs){
+		Event.currentEvent(function(err, docs){
 			if (err) res.render('DB error', 500);
 			res.render('index', {
 				title: 'Daniels'
-				, event: docs.length > 0 ? docs[0] : new Event({})
+				, event: docs.length > 0 ? docs[0] : null
 			});
-		})
+		});
 	});
 
 }
--- app/models/event.js
+++ app/models/event.js
@@ -7,4 +7,8 @@ var EventSchema = new Schema({
 	createdAt: { type: Date, default: Date.now() },
 });
 
+EventSchema.statics.currentEvent = function currentEvent(callback){
+	this.where('end').gte(new Date()).sort('end', -1).limit(1).populate('venue').run(callback);
+}
+
 module.exports = mongoose.model('Event', EventSchema);
