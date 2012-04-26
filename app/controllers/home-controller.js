6bd1138210644e650f588757083bb8c6af0f5f4f
aledalgrande <aledalgrande@gmail.com>
2012-04-26 22:37:37 +0100

    correcting a whooops in a route

--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -12,7 +12,7 @@ module.exports = function(app){
 		});
 	});
 	
-	app.get('/herenow', express.basicAuth(username, password), function(req, res){
+	app.get('/herenow', function(req, res){
 		var https = require('https');
 		https.get({host: 'api.foursquare.com', path: '/v2/venues/4ad7a112f964a520050d21e3/herenow?client_id=FWKRHCAIISGA3ZNMBAX2N52VLOHPDXZTRHZGNRSZ1EUNTZAH&client_secret=MY35MABVVUVG0RPLPP0JLJDKPQ1YXQHZ1WGK1ILHOOBXC4UB&v=20120426'}, function(remote){
 			remote.on('data', function(data) {
