25b3f072547d59d06a466ef3c0722a26f351940f
aledalgrande <aledalgrande@gmail.com>
2012-04-27 19:23:57 +0100

    time left helper

--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -1,7 +1,7 @@
 var Event = mongoose.model('Event')
 
 module.exports = function(app){
-	
+
 	app.get('/', function(req, res){
 		Event.currentEvent(function(err, docs){
 			if (err) res.render('DB error', 500);
@@ -10,6 +10,25 @@ module.exports = function(app){
 				, event: docs.length > 0 ? docs[0] : null
 			});
 		});
+	});
+
+	app.helpers({
+		timeLeft: function(event){
+			var now = new Date();
+			var difference = event.start-now;
+			if (difference > 8.64e7){
+				var days = Math.round(difference/8.64e7);
+				return 'about ' + days + ' day' + (days > 1 ? 's' : '') + ' to go';
+			}
+			else if (difference > 3.6e6){
+				var hours = Math.round(difference/3.6e6);
+				return 'about ' + hours + ' hour' + (hours > 1 ? 's' : '') + ' to go';
+			}
+			else{
+				var minutes = Math.round(difference/6e4);
+				return 'about ' + minutes + ' minute' + (minutes > 1 ? 's' : '') + ' to go';
+			}
+		}
 	});
 
 }
